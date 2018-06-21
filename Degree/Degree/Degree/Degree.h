160e8d3d1db44229be27436bd35cf8f275cceb01
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-06-22 00:09:45 +0300

    Implemented demo project

---
+++ Degree/Degree/Degree/Degree.h
@@ -0,0 +1,19 @@
+//
+//  Degree.h
+//  Degree
+//
+//  Created by Igor Matyushkin on 21.06.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Degree.
+FOUNDATION_EXPORT double DegreeVersionNumber;
+
+//! Project version string for Degree.
+FOUNDATION_EXPORT const unsigned char DegreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Degree/PublicHeader.h>
+
+
---
+++ Degree/DegreeDemo/DegreeDemo/ViewControllers/Main/MainViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MainViewController.swift
+//  DegreeDemo
+//
+//  Created by Igor Matyushkin on 21.06.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+import Degree
+
+class MainViewController: UIViewController {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Outlets
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set gradient
+        
+        let gradientView = self.view as! GradientView
+        gradientView.gradient = Gradient.make
+            .from(.topLeft)
+            .to(.bottomRight)
+            .step(withColor: .white, andLocation: 0.0)
+            .step(withColor: .orange, andLocation: 0.5)
+            .step(withColor: .purple, andLocation: 1.0)
+            .get
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Private object methods
+    
+    // MARK: Actions
+    
+    // MARK: Protocol implementation
+    
+}
